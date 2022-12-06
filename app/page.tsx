e12e7094dc986206254347845287232ba7b28683
Zebeqo <z715596546@qq.com>
2022-12-06 21:23:01 +0800

    feat: sign in

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
-  return <h1 className="text-3xl font-bold underline ?">Hello, Next.js!</h1>;
+import { unstable_getServerSession } from "next-auth";
+import { SignIn } from "./SignIn";
+import * as console from "console";
+
+export default async function Page() {
+  const session = await unstable_getServerSession();
+  console.log(session);
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline ?">Hello, Next.js!</h1>
+      <SignIn />
+      <div>{JSON.stringify(session, null, 2)}</div>
+    </>
+  );
 }
