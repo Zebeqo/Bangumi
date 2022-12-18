2f7a2165725a8ac1db38a4e39a873a255e9b07e1
Zebeqo <z715596546@qq.com>
2022-12-19 04:59:13 +0800

    chore: add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,36 @@
+const path = require("path");
+module.exports = {
+  stories: [
+    {
+      // 👇 The directory field sets the directory your stories
+      directory: "../ui",
+      // 👇 The titlePrefix field will generate automatic titles for your stories
+      titlePrefix: "UI",
+      // 👇 Storybook will load all files that contain the stories extension
+      files: "*.stories.*",
+    },
+    {
+      // 👇 The directory field sets the directory your stories
+      directory: "../components",
+      // 👇 The titlePrefix field will generate automatic titles for your stories
+      titlePrefix: "Components",
+      // 👇 Storybook will load all files that contain the stories extension
+      files: "*.stories.*",
+    },
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-storysource",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {
+      nextConfigPath: path.resolve(__dirname, "../next.config.js"),
+    },
+  },
+  docs: {
+    docsPage: true,
+  },
+};
