da80897b29118df19333237478548fe5018a562a
Zebeqo <z715596546@qq.com>
2022-12-22 22:26:22 +0800

    refactor: clean up

--- components/auth/SessionProvider.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import type { SessionProviderProps } from "next-auth/react";
-import { SessionProvider } from "next-auth/react";
-
-export function ClientSessionProvider(props: SessionProviderProps) {
-  return <SessionProvider {...props} />;
-}
