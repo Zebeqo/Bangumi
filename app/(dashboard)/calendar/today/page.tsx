8e79403c03d1cc5b3db2bde2e58edef8d45b6e46
Zebeqo <z715596546@qq.com>
2022-12-30 21:12:12 +0800

    feat: calendar day page

--- app/(dashboard)/calendar/today/page.tsx
+++ app/(dashboard)/calendar/today/page.tsx
@@ -1,37 +1,6 @@
-import Image from "next/image";
 import { Card } from "@/components/Card";
-import { z } from "zod";
-const calendarScheme = z.array(
-  z.object({
-    weekday: z.object({
-      id: z.number(),
-    }),
-    items: z.array(
-      z.object({
-        id: z.number(),
-        name: z.string(),
-        name_cn: z.string(),
-        air_date: z.string(),
-        rating: z
-          .object({
-            score: z.number(),
-            total: z.number(),
-          })
-          .optional(),
-        images: z
-          .object({
-            large: z.string().url(),
-          })
-          .nullable(),
-        collection: z
-          .object({
-            doing: z.number(),
-          })
-          .optional(),
-      })
-    ),
-  })
-);
+import type { SearchParams } from "@/lib/calendar";
+import { calendarScheme, sortCalendarData } from "@/lib/calendar";
 
 async function getCalendarData() {
   return calendarScheme.parse(
@@ -41,11 +10,6 @@ async function getCalendarData() {
       },
     }).then((res) => res.json())
   );
-}
-
-interface SearchParams {
-  sort?: "do" | "count" | "rating";
-  order?: "asc" | "desc";
 }
 
 export default async function Page({
@@ -58,30 +22,14 @@ export default async function Page({
     (data) => data.weekday.id === [7, 1, 2, 3, 4, 5, 6].at(new Date().getDay())
   );
 
-  const sortedData = todayData?.items.sort((a, b) => {
-    if (searchParams.sort === "do") {
-      return searchParams.order === "asc"
-        ? (a.collection?.doing ?? 0) - (b.collection?.doing ?? 0)
-        : (b.collection?.doing ?? 0) - (a.collection?.doing ?? 0);
-    }
-    if (searchParams.sort === "count") {
-      return searchParams.order === "asc"
-        ? (a.rating?.total ?? 0) - (b.rating?.total ?? 0)
-        : (b.rating?.total ?? 0) - (a.rating?.total ?? 0);
-    }
-    if (searchParams.sort === "rating") {
-      return searchParams.order === "asc"
-        ? (a.rating?.score ?? 0) - (b.rating?.score ?? 0)
-        : (b.rating?.score ?? 0) - (a.rating?.score ?? 0);
-    }
-    return searchParams.order === "asc"
-      ? (a.collection?.doing ?? 0) - (b.collection?.doing ?? 0)
-      : (b.collection?.doing ?? 0) - (a.collection?.doing ?? 0);
-  });
+  if (!todayData) {
+    throw new Error("Failed to fetch data");
+  }
 
+  const sortedData = sortCalendarData(todayData, searchParams);
   return (
     <>
-      {sortedData?.map((item) => (
+      {sortedData.map((item) => (
         /* @ts-expect-error Server Component */
         <Card key={item.id} id={item.id} />
       ))}
