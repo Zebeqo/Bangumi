849344ede9c9affb67d71fe150dc117a6f18c5dd
Zebeqo <z715596546@qq.com>
2023-01-03 15:36:57 +0800

    feat: support tag count

--- ui/Badget.tsx
+++ ui/Badget.tsx
@@ -2,16 +2,22 @@ import type { Color } from "@/lib/colorWrapper";
 import { colorWrapper } from "@/lib/colorWrapper";
 
 export interface BadgesProps {
-  color: Color;
+  color?: Color;
   label: string;
+  count?: number;
 }
-export const Badges: React.FC<BadgesProps> = ({ color, label }) => (
+export const Badges: React.FC<BadgesProps> = ({
+  color = "primary",
+  label,
+  count,
+}) => (
   <div
     className={colorWrapper(
-      "mr-2 inline-flex items-center justify-center rounded-md bg-primary-4 px-3 py-1 text-xs font-medium text-primary-11",
+      "mr-2 inline-flex items-center justify-center space-x-1 rounded-md bg-primary-4 px-3 py-1 text-xs font-medium text-primary-11",
       color
     )}
   >
     <span>{label}</span>
+    {count && <span className="text-neutral-11">{count}</span>}
   </div>
 );
