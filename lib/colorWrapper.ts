893d2a5d8e1459b519b8c94abc6dbcb7b4e1fb6b
Zebeqo <z715596546@qq.com>
2023-01-05 00:13:03 +0800

    style: add semantic color

--- lib/colorWrapper.ts
+++ lib/colorWrapper.ts
@@ -1,4 +1,4 @@
-export type Color = "primary" | "accent" | "neutral";
+export type Color = "primary" | "accent" | "neutral" | "success" | "error";
 export const colorWrapper = (baseProp: string, color: Color) => {
   return baseProp.replaceAll("primary", color);
 };
