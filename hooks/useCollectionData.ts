b73c392b1ab47ce07cfb17a8108a0b8da4ba5314
Zebeqo <z715596546@qq.com>
2023-01-05 22:01:40 +0800

    feat: extract useCollectionData and useSubjectData hooks

---
+++ hooks/useCollectionData.ts
@@ -0,0 +1,46 @@
+import { useQuery } from "@tanstack/react-query";
+import { collectionScheme } from "@/lib/collection";
+import { errorScheme } from "@/lib/error";
+import { useSession } from "next-auth/react";
+import { useErrorToast } from "@/hooks/useErrorToast";
+
+export function useCollectionData(id?: number) {
+  const { data: session } = useSession();
+  const openErrorToast = useErrorToast();
+
+  const { data } = useQuery({
+    queryKey: ["collection", id, session?.user.id],
+    queryFn: async () => {
+      if (!id || !session?.user.id) {
+        return null;
+      }
+      try {
+        const response = await fetch(
+          `https://api.bgm.tv/v0/users/${session.user.id}/collections/${id}`
+        );
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const data = await response.json();
+        const collectionResult = collectionScheme.safeParse(data);
+        if (!collectionResult.success) {
+          const errorResult = errorScheme.safeParse(data);
+          if (errorResult.success) {
+            return null;
+          } else {
+            throw new Error(
+              `FROM ERROR:\n${errorResult.error.message}\n\nFROM COLLECTION:\n${collectionResult.error.message}`
+            );
+          }
+        } else {
+          return collectionResult.data;
+        }
+      } catch (e) {
+        if (e instanceof Error) {
+          const message = e.message;
+          openErrorToast("获取收藏信息失败", message);
+        }
+      }
+    },
+  });
+
+  return data;
+}
---
+++ hooks/useSubjectData.ts
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import { subjectScheme } from "@/lib/subject";
+import { useErrorToast } from "@/hooks/useErrorToast";
+
+export function useSubjectData(id?: number) {
+  const openErrorToast = useErrorToast();
+  const { data } = useQuery({
+    queryKey: ["subject", id],
+    queryFn: async () => {
+      if (!id) {
+        return null;
+      }
+      try {
+        const response = await fetch(`https://api.bgm.tv/v0/subjects/${id}`);
+        return subjectScheme.parse(await response.json());
+      } catch (e) {
+        if (e instanceof Error) {
+          const message = e.message;
+          openErrorToast("获取条目信息失败", message);
+        }
+      }
+    },
+  });
+  return data;
+}
