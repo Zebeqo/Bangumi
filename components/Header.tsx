721b74590a76bd69cdad90891d76abddf1b9a2f6
Zebeqo <z715596546@qq.com>
2023-01-06 02:20:57 +0800

    style(Header): remove transparent

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,7 @@ import { authOptions } from "@/lib/auth";
 export async function Header() {
   const session = await unstable_getServerSession(authOptions);
   return (
-    <div className="sticky top-0 z-10 flex h-16 w-full items-center justify-between border-b border-primary-6 px-6">
+    <div className="sticky top-0 z-10 flex h-16 w-full items-center justify-between border-b border-primary-6 bg-neutral-1 px-6">
       <BangumiLogo />
       <div className="flex items-center justify-center space-x-4 py-2 px-2">
         <div>
