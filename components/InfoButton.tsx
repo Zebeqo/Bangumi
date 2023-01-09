ee23da3cb9b19ab739169daaf89caecbdae34760
Zebeqo <z715596546@qq.com>
2023-01-10 06:02:13 +0800

    fix(storybook): opened panel block on test-storybook command

--- components/InfoButton.tsx
+++ components/InfoButton.tsx
@@ -5,21 +5,32 @@ import { Button } from "@/ui/Button";
 import { useSetAtom } from "jotai/react";
 import { isOpenPanelAtom, panelAtom } from "@/lib/panel";
 import { showFullInfoAtom } from "@/components/panel/Panel";
+import { forwardRef } from "react";
 
-export function InfoButton({ subject_id }: { subject_id: number }) {
-  const setPanel = useSetAtom(panelAtom);
-  const setShowFullInfo = useSetAtom(showFullInfoAtom);
-  const setIsOpenPanel = useSetAtom(isOpenPanelAtom);
-  return (
-    <Button
-      color="accent"
-      type="primary"
-      icon={<InformationCircleIcon />}
-      onClick={() => {
-        setIsOpenPanel(true);
-        setPanel({ subject_id: subject_id, type: "info" });
-        setShowFullInfo(false);
-      }}
-    />
-  );
+interface props extends React.HTMLAttributes<HTMLButtonElement> {
+  subject_id: number;
 }
+export const InfoButton = forwardRef<HTMLButtonElement, props>(
+  ({ subject_id, ...props }, ref) => {
+    const setPanel = useSetAtom(panelAtom);
+    const setShowFullInfo = useSetAtom(showFullInfoAtom);
+    const setIsOpenPanel = useSetAtom(isOpenPanelAtom);
+    return (
+      <Button
+        colorType="accent"
+        type="primary"
+        icon={<InformationCircleIcon />}
+        ref={ref}
+        onClick={() => {
+          setIsOpenPanel(true);
+          setPanel({ subject_id: subject_id, type: "info" });
+          setShowFullInfo(false);
+        }}
+        aria-label="open info panel"
+        {...props}
+      />
+    );
+  }
+);
+
+InfoButton.displayName = "InfoButton";
