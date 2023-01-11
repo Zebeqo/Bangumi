469bb10797cc89591551f781538a76ae6683c66c
Zebeqo <z715596546@qq.com>
2023-01-12 02:09:06 +0800

    feat(EpisodeButton): better error handling

--- lib/error.ts
+++ lib/error.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import type { ToastAction } from "@/lib/toast";
 
 export const errorScheme = z.object({
   title: z.string(),
@@ -10,3 +11,19 @@ export const errorScheme = z.object({
     .optional(),
   description: z.string(),
 });
+
+export class ToastError extends Error {
+  public action: ToastAction;
+  public description: string;
+  constructor({
+    description,
+    action,
+  }: {
+    action: ToastAction;
+    description: string;
+  }) {
+    super(description);
+    this.action = action; // this property is defined in parent
+    this.description = description;
+  }
+}
