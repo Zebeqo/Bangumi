651e06b6b00da8b47253ef6a538c7241f92cc1a5
Zebeqo <z715596546@qq.com>
2023-01-15 02:23:11 +0800

    refactor(PersonList): more concise implementation

--- components/Panel/PersonList.tsx
+++ components/Panel/PersonList.tsx
@@ -3,48 +3,29 @@
 import { ListHeader } from "@/components/Panel/ListHeader";
 import { useSubjectPersonsData } from "@/hooks/use-person";
 import { PersonAvatarCard } from "@/components/Panel/PersonAvatarCard";
+import { personRelationScheme, personRelationWeight } from "@/lib/person";
 
 export function PersonList({ subject_id }: { subject_id: number }) {
   const { data: subjectPersonsData } = useSubjectPersonsData(subject_id);
 
-  const directorData =
-    subjectPersonsData?.filter(
-      (personData) => personData.relation === "导演"
-    ) ?? [];
-  const originalCreatorData =
-    subjectPersonsData?.filter(
-      (personData) => personData.relation === "原作"
-    ) ?? [];
-  const animationProductionData =
-    subjectPersonsData?.filter(
-      (personData) => personData.relation === "动画制作"
-    ) ?? [];
-  const characterSettingData =
-    subjectPersonsData?.filter(
-      (personData) => personData.relation === "人物设定"
-    ) ?? [];
-  const musicProducerData =
-    subjectPersonsData?.filter(
-      (personData) => personData.relation === "音乐"
-    ) ?? [];
+  const sortedListData = subjectPersonsData?.sort((a, b) => {
+    const personRelationA = personRelationScheme.parse(a.relation);
+    const personRelationB = personRelationScheme.parse(b.relation);
 
-  // An array include above arrays data in order
-  const listData = [
-    ...directorData,
-    ...originalCreatorData,
-    ...animationProductionData,
-    ...characterSettingData,
-    ...musicProducerData,
-  ];
+    return (
+      personRelationWeight[personRelationB] -
+      personRelationWeight[personRelationA]
+    );
+  });
 
   return (
     <>
       <div className="flex flex-col space-y-2 p-2">
-        {subjectPersonsData && (
+        {sortedListData && (
           <>
             <ListHeader title={"制作人员"} />
             <div className="grid grid-cols-5 gap-4 px-8 py-2">
-              {listData.slice(0, 5).map((personData) => {
+              {sortedListData.slice(0, 5).map((personData) => {
                 return (
                   <PersonAvatarCard
                     key={personData.id}
