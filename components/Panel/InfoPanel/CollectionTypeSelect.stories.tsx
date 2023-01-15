7b03459f835811c767a0c38bc0135e9263896ad3
Zebeqo <z715596546@qq.com>
2023-01-16 03:22:13 +0800

    fix(storybook): put the query that needs fetching in `waitFor` fn

--- components/Panel/InfoPanel/CollectionTypeSelect.stories.tsx
+++ components/Panel/InfoPanel/CollectionTypeSelect.stories.tsx
@@ -2,7 +2,7 @@ import { expect } from "@storybook/jest";
 import type { Meta, StoryObj } from "@storybook/react";
 import { CollectionTypeSelect as CollectionTypeSelectComponent } from "@/components/Panel/InfoPanel/CollectionTypeSelect";
 import { STORYBOOK_SUBJECT_ID } from "@/lib/constant";
-import { userEvent, within, screen } from "@storybook/testing-library";
+import { userEvent, within, screen, waitFor } from "@storybook/testing-library";
 import { reactQueryDevtoolsDecorator } from "@/lib/storybook";
 
 const meta: Meta<typeof CollectionTypeSelectComponent> = {
@@ -20,22 +20,22 @@ export const CollectionTypeSelect: Story = {
   },
   play: async ({ canvasElement }: { canvasElement: HTMLElement }) => {
     const canvas = within(canvasElement);
-    const selectEl = await canvas.findByRole(
-      "combobox",
-      { name: "Select" },
-      { timeout: 5000 }
+
+    await waitFor(
+      async () => {
+        const selectEl = await canvas.getByRole("combobox", { name: "Select" });
+
+        await expect(selectEl).toHaveTextContent(/./);
+        await userEvent.click(selectEl);
+        const listbox = within(screen.getByRole("listbox"));
+        const options = await listbox.findAllByRole("option");
+        await expect(options.length).toEqual(5);
+        for (const option of options) {
+          await expect(option).toHaveTextContent(/./);
+        }
+      },
+      { timeout: 5000, interval: 1000 }
     );
-    // expect selectEl have one or more character
-    await expect(selectEl).toHaveTextContent(/./);
-
-    await userEvent.click(selectEl);
-    const listbox = within(screen.getByRole("listbox"));
-    const options = await listbox.findAllByRole("option");
-    await expect(options.length).toEqual(5);
-
-    for (const option of options) {
-      await expect(option).toHaveTextContent(/./);
-    }
 
     await userEvent.keyboard("{esc}");
   },
