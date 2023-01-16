f463aff25750db6387b3011f9826f7e1d738da44
Zebeqo <z715596546@qq.com>
2023-01-17 01:38:21 +0800

    feat(font): include `700` weight for `font-bold`

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { StateProvider } from "@/components/provider/StateProvider";
 
 const notoSansSC = Noto_Sans_SC({
   variable: "--font-noto-sans-sc",
-  weight: ["400", "500"],
+  weight: ["400", "500", "700"],
   subsets: ["chinese-simplified", "latin"],
   display: "auto",
 });
