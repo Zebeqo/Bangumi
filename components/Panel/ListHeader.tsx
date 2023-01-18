fa5150a7812a96bb05f5a1d5d1703b3e3dcbc99f
Zebeqo <z715596546@qq.com>
2023-01-19 04:55:06 +0800

    feat(ListHeader): auto hide action text

--- components/Panel/ListHeader.tsx
+++ components/Panel/ListHeader.tsx
@@ -1,19 +1,23 @@
 export const ListHeader = ({
   title,
+  showAction = true,
   onClickAction,
 }: {
   title: string;
+  showAction?: boolean;
   onClickAction?: () => void;
 }) => {
   return (
     <div className="flex items-center justify-between px-6 py-3">
       <span className="text-3xl font-medium text-neutral-12">{title}</span>
-      <span
-        className="cursor-pointer font-medium text-neutral-11"
-        onClick={onClickAction}
-      >
-        显示全部
-      </span>
+      {showAction && (
+        <span
+          className="cursor-pointer font-medium text-neutral-11"
+          onClick={onClickAction}
+        >
+          显示全部
+        </span>
+      )}
     </div>
   );
 };
