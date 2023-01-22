cb4db65f38ca92b4637d6a8d86bd70c49dc48eed
Zebeqo <z715596546@qq.com>
2023-01-22 21:42:17 +0800

    style: update dark mode overlay

--- components/Panel/Panel.tsx
+++ components/Panel/Panel.tsx
@@ -47,7 +47,7 @@ export function Panel() {
             <DialogPrimitive.Overlay
               forceMount
               id="panel-overlay"
-              className="fixed inset-0 z-40 grid place-items-center overflow-y-auto bg-blackA-9 pb-16"
+              className="fixed inset-0 z-40 grid place-items-center overflow-y-auto bg-blackA-9 pb-16 dark:bg-whiteA-9"
             >
               <Transition.Child
                 as={Fragment}
