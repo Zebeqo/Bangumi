126bf80a792ec915d919073fb675292c81450cad
Zebeqo <z715596546@qq.com>
2023-01-22 11:04:05 +0800

    fix: turn system theme support off as it may cause problems

--- components/provider/ThemeProvider.tsx
+++ components/provider/ThemeProvider.tsx
@@ -3,5 +3,9 @@
 import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  return (
+    <ThemeProvider attribute="class" enableSystem={false}>
+      {children}
+    </ThemeProvider>
+  );
 }
