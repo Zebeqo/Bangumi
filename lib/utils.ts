103bd5b8c34108d9662291ed1aead93aec6ae4af
Zebeqo <z715596546@qq.com>
2023-02-03 00:43:53 +0800

    fix: add line-clamp twMerge class group

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,9 +1,26 @@
 import type { ClassValue } from "clsx";
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { extendTailwindMerge } from "tailwind-merge";
 
+const customTwMerge = extendTailwindMerge({
+  classGroups: {
+    lineClamp: [
+      "line-clamp-1",
+      "line-clamp-2",
+      "line-clamp-3",
+      "line-clamp-4",
+      "line-clamp-5",
+      "line-clamp-6",
+      "line-clamp-7",
+      "line-clamp-8",
+      "line-clamp-9",
+      "line-clamp-10",
+    ],
+  },
+});
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return customTwMerge(clsx(inputs));
 }
 
 export function panelScrollToTop() {
