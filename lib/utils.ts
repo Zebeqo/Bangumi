ddabde210eb8329e27474782eb8ac32cbebc573e
Zebeqo <z715596546@qq.com>
2023-02-03 10:46:52 +0800

    refactor: clean up

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,5 @@
 import type { ClassValue } from "clsx";
 import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
 import { extendTailwindMerge } from "tailwind-merge";
 
 const customTwMerge = extendTailwindMerge({
