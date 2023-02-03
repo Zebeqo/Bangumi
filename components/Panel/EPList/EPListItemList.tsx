6c5df9d286b316eec65887d68045801ef74476f1
Zebeqo <z715596546@qq.com>
2023-02-03 18:08:55 +0800

    refactor: rewrite EPItem

--- components/Panel/EPList/EPListItemList.tsx
+++ components/Panel/EPList/EPListItemList.tsx
@@ -2,16 +2,16 @@
 
 import type { z } from "zod";
 import type { episodesPageScheme } from "@/lib/api/episode";
-import { cn } from "@/lib/utils";
-import {
-  CalendarDaysIcon,
-  ChatBubbleLeftRightIcon,
-  ClockIcon,
-} from "@heroicons/react/20/solid";
 import { useCollectionData } from "@/hooks/use-collection";
 import { useEpisodeMutation } from "@/hooks/use-episode";
 import { useToast } from "@/hooks/use-toast";
-import { GhostButton, SelectedButton } from "@/ui/primitive/Button";
+import {
+  EPItem,
+  EPItemComment,
+  EPItemIndex,
+  EPItemInfo,
+  EPItemLeftContent,
+} from "@/ui/primitive/EPItem";
 
 export function EPListItemList({
   episodesData,
@@ -23,105 +23,59 @@ export function EPListItemList({
   const { data: collectionData } = useCollectionData(subject_id);
   const mutateEpisode = useEpisodeMutation();
   const openToast = useToast();
+
   return (
     <>
       {episodesData?.map((episodeData) => (
-        <EPItem
-          key={episodeData.ep}
-          episodeData={episodeData}
-          isSelected={
-            collectionData ? episodeData.ep <= collectionData.ep_status : false
-          }
-          onClickIndex={() => {
-            if (collectionData) {
-              collectionData.ep_status === episodeData.ep
-                ? mutateEpisode.mutate({
-                    subject_id,
-                    currentEp: collectionData.ep_status,
-                    targetEp: episodeData.ep - 1,
-                    subject_type: collectionData.subject.type,
-                    collection_type: collectionData.type,
-                  })
-                : mutateEpisode.mutate({
-                    subject_id,
-                    currentEp: collectionData.ep_status,
-                    targetEp: episodeData.ep,
-                    subject_type: collectionData.subject.type,
-                    collection_type: collectionData.type,
+        <EPItem key={episodeData.ep}>
+          <EPItemLeftContent>
+            <EPItemIndex
+              isSelected={
+                collectionData
+                  ? episodeData.ep <= collectionData.ep_status
+                  : false
+              }
+              value={episodeData.ep}
+              onClick={() => {
+                if (collectionData) {
+                  collectionData.ep_status === episodeData.ep
+                    ? mutateEpisode.mutate({
+                        subject_id,
+                        currentEp: collectionData.ep_status,
+                        targetEp: episodeData.ep - 1,
+                        subject_type: collectionData.subject.type,
+                        collection_type: collectionData.type,
+                      })
+                    : mutateEpisode.mutate({
+                        subject_id,
+                        currentEp: collectionData.ep_status,
+                        targetEp: episodeData.ep,
+                        subject_type: collectionData.subject.type,
+                        collection_type: collectionData.type,
+                      });
+                } else {
+                  openToast({
+                    type: "info",
+                    title: "请先收藏该条目",
                   });
-            } else {
-              openToast({
-                type: "info",
-                title: "请先收藏该条目",
-              });
-            }
-          }}
-          onClickBubble={() => {
-            window.open(`https://bgm.tv/ep/${episodeData.id}`, "_blank");
-          }}
-        />
+                }
+              }}
+            />
+            <EPItemInfo
+              name={episodeData.name || "未知"}
+              name_cn={episodeData.name === "" ? "" : episodeData.name_cn}
+              airdate={episodeData.airdate}
+              duration={episodeData.duration}
+            />
+          </EPItemLeftContent>
+          <EPItemComment
+            count={episodeData.comment}
+            onClick={() => {
+              window.open(`https://bgm.tv/ep/${episodeData.id}`, "_blank");
+            }}
+          />
+        </EPItem>
       ))}
     </>
-  );
-}
-
-function EPItem({
-  episodeData,
-  isSelected,
-  onClickIndex,
-  onClickBubble,
-}: {
-  episodeData: z.infer<typeof episodesPageScheme.shape.data.element>;
-  isSelected: boolean;
-  onClickIndex?: () => void;
-  onClickBubble?: () => void;
-}) {
-  return (
-    <div className="flex items-center justify-between px-2">
-      <div className="flex items-center space-x-4 p-2">
-        {isSelected ? (
-          <SelectedButton colorType={"neutral"} onClick={onClickIndex}>
-            {episodeData.ep}
-          </SelectedButton>
-        ) : (
-          <GhostButton colorType={"neutral"} onClick={onClickIndex}>
-            {episodeData.ep}
-          </GhostButton>
-        )}
-        <div className="flex flex-col space-y-1">
-          <div className="flex items-center font-medium">
-            <span className="text-xl text-neutral-12">
-              {episodeData.name || "未知"}
-            </span>
-            <span
-              className={cn(
-                "text-neutral-11",
-                episodeData.name === "" && "hidden"
-              )}
-            >
-              {episodeData.name_cn === "" ? "" : `（${episodeData.name_cn}）`}
-            </span>
-          </div>
-          <div className="flex space-x-2 text-xs font-medium text-neutral-11">
-            <div className="flex justify-center space-x-1">
-              <CalendarDaysIcon className="h-4 w-4" />
-              <span>{episodeData.airdate}</span>
-            </div>
-            <div className="flex justify-center space-x-1">
-              <ClockIcon className="h-4 w-4" />
-              <span>{episodeData.duration}</span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <GhostButton
-        colorType={"neutral"}
-        onClick={onClickBubble}
-        aria-label="comment"
-      >
-        <ChatBubbleLeftRightIcon className="mr-2 h-5 w-5" />
-        {episodeData.comment.toString()}
-      </GhostButton>
-    </div>
   );
 }
---
+++ ui/primitive/EPItem.stories.tsx
@@ -0,0 +1,63 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import {
+  EPItem,
+  EPItemComment,
+  EPItemIndex,
+  EPItemInfo,
+  EPItemLeftContent,
+} from "@/ui/primitive/EPItem";
+import { action } from "@storybook/addon-actions";
+
+const meta: Meta = {
+  title: "EPItem",
+};
+
+export default meta;
+
+export const EPItem_: StoryObj<{
+  isSelected: boolean;
+  index: number;
+  name: string;
+  name_cn: string;
+  airdate: string;
+  duration: string;
+  commentCount: number;
+}> = {
+  args: {
+    isSelected: false,
+    index: 3,
+    name: "THE BLOOD WARFARE",
+    name_cn: "血战",
+    airdate: "2022-10-10",
+    duration: "00:24:07",
+    commentCount: 125,
+  },
+  render: ({
+    isSelected,
+    index,
+    name,
+    name_cn,
+    airdate,
+    duration,
+    commentCount,
+  }) => (
+    <div className="container w-screen">
+      <EPItem>
+        <EPItemLeftContent>
+          <EPItemIndex
+            value={index}
+            isSelected={isSelected}
+            onClick={action("click index")}
+          />
+          <EPItemInfo
+            name={name}
+            name_cn={name_cn}
+            airdate={airdate}
+            duration={duration}
+          />
+        </EPItemLeftContent>
+        <EPItemComment count={commentCount} onClick={action("click comment")} />
+      </EPItem>
+    </div>
+  ),
+};
