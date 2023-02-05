603a053b127c2ef940708acd3814d6f58fce0ddf
Zebeqo <z715596546@qq.com>
2023-02-06 00:42:18 +0800

    chore: deploy storybook manual

---
+++ storybook-static/sb-addons/storybook-11/manager-bundle.mjs
@@ -0,0 +1,4 @@
+try{
+var s=__STORYBOOKAPI__,{ActiveTabs:a,Consumer:n,ManagerContext:u,Provider:S,addons:r,combineParameters:c,controlOrMetaKey:i,controlOrMetaSymbol:l,eventMatchesShortcut:m,eventToShortcut:h,isMacLike:p,isShortcutTaken:y,keyToSymbol:d,merge:b,mockChannel:O,optionOrAltSymbol:T,shortcutMatchesShortcut:A,shortcutToHumanString:_,types:g,useAddonState:P,useArgTypes:k,useArgs:M,useChannel:C,useGlobalTypes:v,useGlobals:f,useParameter:x,useSharedState:K,useStoryPrepared:B,useStorybookApi:G,useStorybookState:I}=__STORYBOOKAPI__;r.setConfig({panelPosition:"right"});
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
+//# sourceMappingURL=manager-bundle.mjs.map
