482cea2a58ca7aa382f4ee1fa61f4f69d1fd557d
Zebeqo <z715596546@qq.com>
2023-03-04 22:05:50 +0800

    chore(deps): update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,46 +1,29 @@
 const path = require("path");
 export default {
-  stories: [
-    {
-      // 👇 The directory field sets the directory your stories
-      directory: "../ui",
-      // 👇 The titlePrefix field will generate automatic titles for your stories
-      titlePrefix: "UI",
-      // 👇 Storybook will load all files that contain the stories extension
-      files: "**/*.stories.*",
-    },
-    {
-      // 👇 The directory field sets the directory your stories
-      directory: "../components",
-      // 👇 The titlePrefix field will generate automatic titles for your stories
-      titlePrefix: "Components",
-      // 👇 Storybook will load all files that contain the stories extension
-      files: "**/*.stories.*",
-    },
-    {
-      directory: "../hooks",
-      titlePrefix: "Hooks",
-      files: "**/*.stories.*",
-    },
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-storysource",
-    "storybook-dark-mode",
-    "@tomfreudenberg/next-auth-mock/storybook",
-  ],
+  stories: [{
+    directory: "../ui",
+    titlePrefix: "UI",
+    files: "**/*.@(mdx|stories.*)"
+  }, {
+    directory: "../components",
+    titlePrefix: "Components",
+    files: "**/*.@(mdx|stories.*)"
+  }, {
+    directory: "../hooks",
+    titlePrefix: "Hooks",
+    files: "**/*.@(mdx|stories.*)"
+  }],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-storysource", "storybook-dark-mode", "@tomfreudenberg/next-auth-mock/storybook", "@storybook/addon-mdx-gfm"],
   framework: {
     name: "@storybook/nextjs",
     options: {
-      nextConfigPath: path.resolve(__dirname, "../next.config.js"),
-    },
+      nextConfigPath: path.resolve(__dirname, "../next.config.js")
+    }
   },
   docs: {
-    autodocs: "tag",
+    autodocs: "tag"
   },
   features: {
-    interactionsDebugger: true, // 👈 Enable playback controls
-  },
+    interactionsDebugger: true // 👈 Enable playback controls
+  }
 };
