9166a9a6cdd7f87ab4c2f31c39c36da59689b27e
Zebeqo <z715596546@qq.com>
2023-03-06 00:55:27 +0800

    style: fix Header bg color

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,7 @@ import { authOptions } from "@/lib/auth";
 export async function Header() {
   const session = await getServerSession(authOptions);
   return (
-    <div className="sticky top-0 z-10 flex h-16 w-full items-center justify-between border-b border-primary-6 px-6">
+    <div className="sticky top-0 z-10 flex h-16 w-full items-center justify-between border-b border-primary-6 bg-white px-6 dark:bg-neutral-1">
       <BangumiLogo />
       <div className="flex items-center justify-center space-x-4 py-2 px-2">
         <div>
