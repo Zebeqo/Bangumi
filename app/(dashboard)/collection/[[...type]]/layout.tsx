a7595201733a07ec0a774deb549abd6d6d5749a2
Zebeqo <z715596546@qq.com>
2023-03-08 03:07:50 +0800

    feat: add Metadata

--- app/(dashboard)/collection/[[...type]]/layout.tsx
+++ app/(dashboard)/collection/[[...type]]/layout.tsx
@@ -1,6 +1,43 @@
 import { Subnav } from "@/components/Subnav";
 import { PersonalViewSwitch } from "@/components/Switch/personalViewSwitch";
 
+// eslint-disable-next-line @typescript-eslint/require-await
+export async function generateMetadata({
+  params,
+}: {
+  params: { type?: string[] };
+}) {
+  const selectedTypeValue =
+    params.type?.at(0) === "do"
+      ? "在看"
+      : params.type?.at(0) === "wish"
+      ? "想看"
+      : params.type?.at(0) === "collect"
+      ? "看过"
+      : params.type?.at(0) === "on_hold"
+      ? "搁置"
+      : params.type?.at(0) === "dropped"
+      ? "抛弃"
+      : "在看";
+
+  const collectionTypeValue =
+    params.type?.at(1) === "anime"
+      ? "动画"
+      : params.type?.at(1) === "book"
+      ? "书籍"
+      : params.type?.at(1) === "music"
+      ? "音乐"
+      : params.type?.at(1) === "game"
+      ? "游戏"
+      : params.type?.at(1) === "real"
+      ? "三次元"
+      : "动漫";
+
+  return {
+    title: `${collectionTypeValue} | ${selectedTypeValue}`,
+  };
+}
+
 export default function Layout({
   children,
   params,
--- app/(dashboard)/hot/[[...type]]/layout.tsx
+++ app/(dashboard)/hot/[[...type]]/layout.tsx
@@ -28,6 +28,30 @@ const navItems = [
   },
 ];
 
+// eslint-disable-next-line @typescript-eslint/require-await
+export async function generateMetadata({
+  params,
+}: {
+  params: { type?: string[] };
+}) {
+  const typeValue =
+    params.type?.at(0) === "anime"
+      ? "动画"
+      : params.type?.at(0) === "book"
+      ? "书籍"
+      : params.type?.at(0) === "music"
+      ? "音乐"
+      : params.type?.at(0) === "game"
+      ? "游戏"
+      : params.type?.at(0) === "real"
+      ? "三次元"
+      : "动漫";
+
+  return {
+    title: `热门${typeValue}`,
+  };
+}
+
 export default function Layout({
   children,
   params,
