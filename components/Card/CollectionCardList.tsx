952346b7cb85c744cc18995e768983635b950271
Zebeqo <z715596546@qq.com>
2023-03-20 19:06:19 +0800

    perf: memo page data

--- components/Card/CollectionCardList.tsx
+++ components/Card/CollectionCardList.tsx
@@ -2,7 +2,7 @@
 
 import { useCollectionsPageData } from "@/hooks/use-collection";
 import { useInView } from "react-intersection-observer";
-import { Fragment, useEffect } from "react";
+import { Fragment, useEffect, useMemo } from "react";
 import { LoadMore } from "@/components/LoadMore";
 import { CollectionCard } from "@/components/Card/CollectionCard";
 import { personalViewModeAtom } from "@/components/Switch/personalViewSwitch";
@@ -31,19 +31,23 @@ export function CollectionCardList({
     }
   }, [fetchNextPage, inView]);
 
+  const CardList = useMemo(() => {
+    return collectionsPageData?.pages.map((group, i) => (
+      <Fragment key={i}>
+        {group?.data.map((collection) => (
+          <CollectionCard
+            collection={collection}
+            pvMode={pvMode}
+            key={collection.subject.id}
+          />
+        ))}
+      </Fragment>
+    ));
+  }, [collectionsPageData, pvMode]);
+
   return (
     <>
-      {collectionsPageData?.pages.map((group, i) => (
-        <Fragment key={i}>
-          {group?.data.map((collection) => (
-            <CollectionCard
-              collection={collection}
-              pvMode={pvMode}
-              key={collection.subject.id}
-            />
-          ))}
-        </Fragment>
-      ))}
+      {CardList}
       <div className="absolute inset-x-0 -bottom-12 flex w-full items-center justify-center">
         <LoadMore ref={ref} hasMore={hasNextPage} />
       </div>
