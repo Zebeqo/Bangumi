3c0e4dbd1f2c4f5de04d6ae5e710e8f82401423d
Zebeqo <z715596546@qq.com>
2023-03-21 11:58:28 +0800

    fix(ThemeButton): remove unnecessary loading state

--- components/Button/ThemeButton.tsx
+++ components/Button/ThemeButton.tsx
@@ -3,46 +3,40 @@
 import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
-import { LoadingSpinner } from "@/ui/icon/24/LoadingSpinner";
 import { Button } from "@/ui/primitive/Button";
 import { Tooltip } from "@/ui/primitive/Tooltip";
 
 export function ThemeButton() {
-  const [mounted, setMounted] = useState(false);
-  const { theme, setTheme } = useTheme();
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return (
-      <Button
-        variant={{ type: "ghost", iconOnly: true }}
-        aria-label="Loading Theme"
-        className="hover:bg-transparent active:bg-transparent"
-      >
-        <LoadingSpinner className="h-6 w-6" />
-      </Button>
-    );
-  }
+  const { setTheme } = useTheme();
 
   return (
-    <Tooltip content={theme === "dark" ? "明亮模式" : "黑暗模式"}>
-      <Button
-        variant={{ type: "ghost", iconOnly: true }}
-        aria-label="Toggle Dark Mode"
-        onClick={() => {
-          setTheme(theme === "dark" ? "light" : "dark");
-        }}
-      >
-        {theme === "dark" ? (
-          <SunIcon className="h-6 w-6" />
-        ) : (
-          <MoonIcon className="h-6 w-6" />
-        )}
-      </Button>
-    </Tooltip>
+    <>
+      <span className="hidden dark:inline-block">
+        <Tooltip content={"明亮模式"}>
+          <Button
+            variant={{ type: "ghost", iconOnly: true }}
+            aria-label="Toggle light Mode"
+            onClick={() => {
+              setTheme("light");
+            }}
+          >
+            <SunIcon className="h-6 w-6" />
+          </Button>
+        </Tooltip>
+      </span>
+      <span className="inline-block dark:hidden">
+        <Tooltip content={"黑暗模式"}>
+          <Button
+            variant={{ type: "ghost", iconOnly: true }}
+            aria-label="Toggle dark Mode"
+            onClick={() => {
+              setTheme("dark");
+            }}
+          >
+            <MoonIcon className="h-6 w-6" />
+          </Button>
+        </Tooltip>
+      </span>
+    </>
   );
 }
