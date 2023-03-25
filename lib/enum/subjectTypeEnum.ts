705dea86ca5ece88b71cf687ed2454d0e8dbac4d
Zebeqo <z715596546@qq.com>
2023-03-25 19:40:36 +0800

    refactor: reorder subject type

--- lib/enum/subjectTypeEnum.ts
+++ lib/enum/subjectTypeEnum.ts
@@ -2,8 +2,8 @@ import { objectKeys } from "@/lib/utils";
 import { z } from "zod";
 
 export const subjectTypeEnum = {
-  book: { value: 1, label: "书籍" },
   anime: { value: 2, label: "动画" },
+  book: { value: 1, label: "书籍" },
   music: { value: 3, label: "音乐" },
   game: { value: 4, label: "游戏" },
   real: { value: 6, label: "三次元" },
