2cd247d6246d7dbdd79c739032ce10b578133106
Zebeqo <z715596546@qq.com>
2023-03-26 20:14:55 +0800

    revert: "perf: conditionally render card button"
    This reverts commit decf7a58

--- components/Card/CollectionCard.tsx
+++ components/Card/CollectionCard.tsx
@@ -11,7 +11,7 @@ import { Rating } from "@/components/Rating/Rating";
 import {
   Card,
   CardButtonGroup,
-  CardBody,
+  CardContent,
   CardFooter,
   CardHeader,
   CardImage,
@@ -42,7 +42,7 @@ export function CollectionCard({
           alt={"card-image"}
         />
       </CardImage>
-      <CardBody>
+      <CardContent>
         <CardHeader>
           <CardTitle
             mainTitle={collection.subject.name_cn || collection.subject.name}
@@ -119,7 +119,7 @@ export function CollectionCard({
             )}
           </div>
         </CardFooter>
-      </CardBody>
+      </CardContent>
     </Card>
   );
 }
